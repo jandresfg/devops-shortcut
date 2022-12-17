04d40b633192f3043031552a94ea1c93d480e406
Andres Fernandez <jandresfgz@gmail.com>
2022-12-17 09:00:11 -0500

    feat: create new command "devops" with arg "id" and print it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::{arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("devops")
+        .version("1.0")
+        .author("Andres F. <jandresfgz@gmail.com>")
+        .about("Launch devops ticket in browser by providing its id")
+        .arg(arg!(--id <VALUE>).required(true))
+        .get_matches();
+
+    let id = matches.get_one::<String>("id").expect("required");
+    println!("id: {:?}", id);
 }
